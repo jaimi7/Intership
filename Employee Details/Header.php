3db27bf116400722158900fc0317c548e271ee5e
Jaimi Patel <84757239+jaimi7@users.noreply.github.com>
2021-06-19 09:49:17 +0530

    Add files via upload

--- Employee Details/Header.php
+++ Employee Details/Header.php
@@ -1,16 +1,9 @@
-<html>
-    <head>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-    <div class="header">
-        <ul>
-            <li><h1><i>Employee Detaile Handler</i></h1></li>
-            <li><a href="Home.php">Home</a></li>
-            <li><a href="Add.php">Add</a></li>
-            <li><a href="Update.php">Update</a></li>
-            <li><a href="Delete.php">Delete</a></li>
-        </ul>
-    </div>
-    </body>
-</html>
+<div class="header">
+    <ul>
+        <li><h1><i>Employee Detaile Handler</i></h1></li>
+        <li><a href="Home.php">Home</a></li>
+        <li><a href="Add.php">Add</a></li>
+        <li><a href="Update.php">Update</a></li>
+        <li><a href="Delete.php">Delete</a></li>
+    </ul>
+</div>
